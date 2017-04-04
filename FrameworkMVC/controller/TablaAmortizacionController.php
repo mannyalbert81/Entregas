92d18ed56bbcebabf537481ccf3763ddf8a823e6
STEVEN <STEVEN@DESKTOP-42BTC1D>
2017-04-03 19:55:00 -0500

    sdwefsdf

--- FrameworkMVC/controller/TablaAmortizacionController.php
+++ FrameworkMVC/controller/TablaAmortizacionController.php
@@ -26,13 +26,16 @@ class TablaAmortizacionController extends ControladorBase{
 			if (!empty($resultPer))
 			{
 				
+				$resultAmortizacion=array();
+				$resultDatos=array();
+				$resultRubros=array();
 				
 				if(isset($_POST["buscar"]))
 				{
 				  
-					
 					$identificacion=$_POST['identificacion'];
 						
+					if ($identificacion!=""){
 					
 						$columnas = "fc_clientes.id_clientes,
 								  fc_clientes.ruc_clientes,
@@ -58,14 +61,51 @@ class TablaAmortizacionController extends ControladorBase{
 							
 						$resultRes = $clientes->getCondiciones($columnas, $tablas, $where_to, $id);
 							
-							
 					
-				
+					}
+				}
+					
+					
+		if(isset($_POST["Generar"]))
+					{
+								
+						$interes=0;
+						$total= isset($_POST['total'])?(double)$_POST['total']:0; 
+						$porcentaje_capital=isset($_POST['por_capital'])?(double)$_POST['por_capital']:0;
+						$total_capital=$total-($total*($porcentaje_capital/100));
+						//$fecha_corte=$_POST['fecha_corte'];
+						//$fecha_emision=$_POST['fecha_emision'];
+						$fecha_corte='2017-01-02';
+						$fecha_emision='';
+							
+							
+						array_push($resultDatos,array('total'=> $total,'porcentaje_capital'=>$porcentaje_capital,'total_capital'=>$total_capital));
+							
+						//pruebas tabla amortizacion
+							
+						$saldo_capital=$total-($total*($porcentaje_capital/100));
+						$tasa_interes=8.86;
+						$numero_cuotas=$_POST['plazo_meses_amortizacion_cabeza'];
+							
+						$saldo_honorarios=0;
+							
+						$resultAmortizacion=$this->tablaAmortizacion($saldo_capital, $numero_cuotas, $fecha_corte);
+							
+						$interes=0.812;
+							
+						$dias_mora=$this->diasMora($fecha_corte, $fecha_emision);
+							
+						$resultRubros=$this->tablaRubros($total, $interes, $dias_mora);
+							
+						
+						
+						
+					
 				}
 		
 				
 				$this->view("TablaAmortizacion",array(
-						"resultRes"=>$resultRes
+						"resultRes"=>$resultRes,'resultDatos'=>$resultDatos,'resultAmortizacion'=>$resultAmortizacion,'resultRubros'=>$resultRubros
 			
 				));
 		
@@ -95,222 +135,180 @@ class TablaAmortizacionController extends ControladorBase{
 	
 	}
 	
-	public function InsertaTipoCierre(){
-			
-		session_start();
-
-		
-		$tipo_cierre=new TipoCierreModel();
-		$nombre_controladores = "TipoCierre";
-		$id_rol= $_SESSION['id_rol'];
-		$resultPer = $tipo_cierre->getPermisosEditar("   controladores.nombre_controladores = '$nombre_controladores' AND permisos_rol.id_rol = '$id_rol' " );
-		
-		
-		if (!empty($resultPer))
-		{
-		
-		
-		
-			$resultado = null;
-			$tipo_cierre=new TipoCierreModel();
-		   
-		   	if (isset ($_POST["Guardar"]) )
-				
-							
-			{
-				$_nombre_tipo_cierre = $_POST["nombre_tipo_cierre"];
-				$_id_entidades = $_POST["id_entidades"];
-				
-				if($_nombre_tipo_cierre==""){
-					
-					
-				}
-				else{
-					
-					
-				
-				
-				
-				if(isset($_POST["id_tipo_cierre"])) 
-				{
-					
-					$_id_tipo_cierre = $_POST["id_tipo_cierre"];
-					$colval = " nombre_tipo_cierre = '$_nombre_tipo_cierre'   ";
-					$tabla = "tipo_cierre";
-					$where = "id_tipo_cierre = '$_id_tipo_cierre'    ";
-					
-					$resultado=$tipo_cierre->UpdateBy($colval, $tabla, $where);
-					
-				}else {
-					
-			
-
-				
-				$funcion = "ins_tipo_cierre";
-				
-				$parametros = " '$_nombre_tipo_cierre','$_id_entidades'  ";
-					
-				$tipo_cierre->setFuncion($funcion);
-		
-				$tipo_cierre->setParametros($parametros);
-		
-		
-				$resultado=$tipo_cierre->Insert();
-			 
-				$traza=new TrazasModel();
-				$_nombre_controlador = "Tipo Cierre";
-				$_accion_trazas  = "Guardar";
-				$_parametros_trazas = $_nombre_tipo_cierre;
-				$resultado = $traza->AuditoriaControladores($_accion_trazas, $_parametros_trazas, $_nombre_controlador);
-				
-				}
-			 
-				}
-		
-			}
-			$this->redirect("TipoCierre", "index");
-
-		}
-		else
-		{
-			$this->view("Error",array(
-					
-					"resultado"=>"No tiene Permisos de Insertar tipos de Cierre"
-		
-			));
-		
-		
-		}
-	
-
-		$tipo_cierre=new TipoCierreModel();
-
-		$nombre_controladores = "TipoCierre";
-		$id_rol= $_SESSION['id_rol'];
-		$resultPer = $tipo_cierre->getPermisosEditar("   controladores.nombre_controladores = '$nombre_controladores' AND permisos_rol.id_rol = '$id_rol' " );
-		
-		
-		if (!empty($resultPer))
-		{
-		
-		
-		
-			$resultado = null;
-			$tipo_cierre=new TipoCierreModel();
-		
-			
-			
-			if (isset ($_POST["nombre_tipo_cierre"]) )
-				
-			{
-				$_nombre_tipo_cierre = $_POST["nombre_tipo_cierre"];
-				
-				if(isset($_POST["id_tipo_cierre"]))
-				{
-				$_id_tipo_cierre = $_POST["id_tipo_cierre"];
-				$colval = " nombre_tipo_cierre = '$_nombre_tipo_cierre'   ";
-				$tabla = "tipo_cierre";
-				$where = "id_tipo_cierre = '$_id_tipo_cierre'    ";
-					
-				$resultado=$tipo_cierre->UpdateBy($colval, $tabla, $where);
-					
-				}else {
-				
-			
-				$funcion = "ins_tipo_cierre";
-				
-				$parametros = " '$_nombre_tipo_cierre'  ";
-					
-				$tipo_cierre->setFuncion($funcion);
-		
-				$tipo_cierre->setParametros($parametros);
-		
-		
-				$resultado=$tipo_cierre->Insert();
-			 }
-		
-			}
-			$this->redirect("TipoCierre", "index");
-
-		}
-		else
-		{
-			$this->view("Error",array(
-					
-					"resultado"=>"No tiene Permisos de Insertar tipos de Cierre"
-		
-			));
-		
-		
-		}
-		
-	}
-
-
-
-
-	public function borrarId()
+	public function tablaAmortizacion($saldo_capital,$numero_cuotas,$fecha_corte)
 	{
+		//array donde guardar tabla amortizacion
+		$resultAmortizacion=array();
+	
+	
+		$tasa_interes=8.86;
+	
+		$saldo_honorarios=0;
+		$otros=0;
+		$total_Capital=0;
+		$total_Honorarios=0;
+		$total_Convenio=0;
+		$total_Interes=0;
+	
+	
+		$plazo=$numero_cuotas;
+	
+		$honoraExon = $saldo_honorarios / ($plazo);
+	
+		$porcent = ($tasa_interes / 12)/100;
+			
+		$capinteres = $saldo_capital * (($porcent * (pow((1 + $porcent), ((int)($plazo))))) / (pow((1 + $porcent), ((int)($plazo))) - 1));
+	
+	
+		$inter = 1*$saldo_capital*$porcent;
+	
+		$abono = $capinteres-$inter;
+			
+		$saldocap = $saldo_capital;
+	
+		$cuota = round($capinteres,2)+round($honoraExon,2)+round($otros,2);
+			
+			
+			
+	
+		for( $i = 1; $i <= $plazo; $i++) {
+	
+	
+			$inter = 1*$saldocap*$porcent;
+			$abono = $capinteres-$inter;
+			$saldocap = $saldocap-$abono;
+	
+			$total_Interes = $total_Interes + $inter;
+	
+			$total_Capital = $total_Capital + $abono;
+	
+			$total_Honorarios = $total_Honorarios + $honoraExon;
+	
+			$total_Convenio = $total_Convenio + $cuota;
+	
+			$fecha=strtotime('+1 month',strtotime($fecha_corte));
+	
+			$fecha=date('Y-m-d',$fecha);
+	
+			$fecha_corte=$fecha;
+				
+				
+			$resultAmortizacion['tabla'][]=array(
+					array('periodo'=> $i,
+							'fecha_vencimiento'=>$fecha,
+							'abono_capital'=>$abono,
+							'interes'=>$inter,
+							'capital_interes'=>$capinteres,
+							'saldo_capital'=>$saldocap,
+							'saldo_honorarios'=>$honoraExon,
+							'otros'=>$otros,
+							'cuota'=>$cuota
+					)
+			);
+		}
+	
+		$resultAmortizacion['totales']=array(
+				array('total_capital'=> $total_Capital,
+						'total_interes'=>$total_Interes,
+						'total_honorarios'=>$total_Honorarios,
+						'total_otros'=>$otros,
+						'total_convenio'=>$total_Convenio
+	
+				));
+	
+		return $resultAmortizacion;
+	
+	
+	}
+	
+	
+	public function tablaRubros($saldo_capital,$interes,$dias_mora)
+	{
+		//****rubros
+		//Interés Normal:	Interés Mora:	Costos Operativos (Gastos Cobranza: $0.00):	Capital:
+		//Cuantía:	Mora Coactiva:	Emisión Título C.	Costas Procesales:	Honorarios:	Deuda Total:
+		//****cabeceras
+		//Rubros 	Deuda 	Interes Rebaja	% Rebaja de Intereses	Cuota Inicial 	Saldos
+	
+		$resultRubros=array();
+		$deuda=0;
+		$interes_rebaja=0;
+		$porc_rebaja=0;
+		$cuota_inicial=0;
+		$saldos=0;
+	
+		$mora=($saldo_capital*$interes*12*$dias_mora)/3600;
+		$fila=array('rubros'=>'','deuda'=>$deuda,'interes_rebaja'=>$interes_rebaja,'porc_rebaja'=>$porc_rebaja,'cuota_inicial'=>$cuota_inicial,'saldos'=>$saldos);
+	
+	
+		$rubros=array('interes_normal'=>'Interés Normal:','interes_mora'=>'Interés Mora:','costos_operativos'=>'Costos Operativos(Gastos Cobranza: $0.00):','capital'=>	'Capital:',
+				'cuantia'=>'Cuantía:','mora_coactiva'=>	'Mora Coactiva:','emision_titulo'=>'Emisión Título C:','costos_procesales'=>'Costos Procesales:','honorarios'=>'Honorarios:',
+				'deudatotal'=>'Deuda Total:');
+	
+		$fila['rubros']=$rubros['interes_normal'];
+		$resultRubros['interes_normal']=$fila;
+	
+		$fila['rubros']=$rubros['interes_mora'];
+		$resultRubros['interes_mora']=$fila;
+	
+		$fila['rubros']=$rubros['costos_operativos'];
+		$resultRubros['costos_operativos']=$fila;
+	
+		$fila['rubros']=$rubros['capital'];
+		$fila['deuda']=$saldo_capital;
+		$fila['saldos']=$saldo_capital;
+		$resultRubros['capital']=$fila;
+	
+		$fila['rubros']=$rubros['cuantia'];
+		$fila['deuda']=0;
+		$fila['saldos']=0;
+		$resultRubros['cuantia']=$fila;
+	
+		$fila['rubros']=$rubros['mora_coactiva'];
+		$fila['deuda']=round($mora,2);
+		$fila['saldos']=round($mora,2);
+		$resultRubros['mora_coactiva']=$fila;
+	
+		$fila=array('rubros'=>'','deuda'=>$deuda,'interes_rebaja'=>$interes_rebaja,'porc_rebaja'=>$porc_rebaja,'cuota_inicial'=>$cuota_inicial,'saldos'=>$saldos);
+		$fila['rubros']=$rubros['emision_titulo'];
+		$resultRubros['emision_titulo']=$fila;
+	
+		$fila['rubros']=$rubros['costos_procesales'];
+		$resultRubros['costos_procesales']=$fila;
+	
+		$fila['rubros']=$rubros['honorarios'];
+		$resultRubros['honorarios']=$fila;
+	
+		$fila['rubros']=$rubros['deudatotal'];
+		$resultRubros['deudatotal']=$fila;
+	
+	
+		return $resultRubros;
+	}
+	
+	public function diasMora($fecha_corte,$fecha_emision)
+	{
+	
+		$_fecha_corte=date_create($fecha_corte);
+	
+		$_fecha_emision=date_create($fecha_emision);
+	
+		$dias_mora=date_diff($_fecha_emision, $_fecha_corte);
+	
+		$dias_mora=$dias_mora->format('%a');;
+	
+		return  $dias_mora;
+	}
+	
+	
+	
 
-		session_start();
-		
-		$permisos_rol=new PermisosRolesModel();
-		$nombre_controladores = "Roles";
-		$id_rol= $_SESSION['id_rol'];
-		$resultPer = $permisos_rol->getPermisosEditar("   controladores.nombre_controladores = '$nombre_controladores' AND permisos_rol.id_rol = '$id_rol' " );
-			
-		if (!empty($resultPer))
-		{
-			if(isset($_GET["id_tipo_cierre"]))
-			{
-				$id_tipo_cierre=(int)$_GET["id_tipo_cierre"];
-				
-				$tipo_cierre=new TipoCierreModel();
-				
-				$tipo_cierre->deleteBy(" id_tipo_cierre",$id_tipo_cierre);
-				
-				$traza=new TrazasModel();
-				$_nombre_controlador = "TipoCierre";
-				$_accion_trazas  = "Borrar";
-				$_parametros_trazas = $id_tipo_cierre;
-				$resultado = $traza->AuditoriaControladores($_accion_trazas, $_parametros_trazas, $_nombre_controlador);
-			}
-			
-			$this->redirect("TipoCierre", "index");
-			
-			
-		}
-		else
-		{
-			$this->view("Error",array(
-				"resultado"=>"No tiene Permisos de Borrar Tipos de Cierre"
-			
-			));
-		}
-				
-	}
+
+
+
 	
 	
-	public function Reporte(){
 	
-		//Creamos el objeto usuario
-		$tipo_cierre=new TipoCierreModel();
-		//Conseguimos todos los usuarios
-		
-	
-	
-		session_start();
-	
-	
-		if (isset(  $_SESSION['usuario']) )
-		{
-			$resultRep = $roles->getByPDF("id_rol, nombre_rol", " nombre_rol != '' ");
-			$this->report("TipoCierre",array(	"resultRep"=>$resultRep));
-	
-		}
-					
-	
-	}
 	
 	
 	
