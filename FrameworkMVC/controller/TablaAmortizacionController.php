0214d6d16fe9b7f39aa01e64e22d52e4c4a3b1be
STEVEN TITUAÑA <steven642010@hotmail.com>
2017-04-04 12:48:35 -0500

    dd

--- FrameworkMVC/controller/TablaAmortizacionController.php
+++ FrameworkMVC/controller/TablaAmortizacionController.php
@@ -66,7 +66,7 @@ class TablaAmortizacionController extends ControladorBase{
 				}
 					
 					
-		if(isset($_POST["Generar"]))
+		     	if(isset($_POST["Generar"]))
 					{
 								
 						$interes=0;
