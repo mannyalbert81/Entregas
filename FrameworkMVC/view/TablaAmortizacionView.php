2f84c6fb6253c43098dbca28547cad51bafa8d12
desarrollo <desarrollo@masoft.net>
2017-04-05 11:34:37 -0500

    gbbb

--- FrameworkMVC/view/TablaAmortizacionView.php
+++ FrameworkMVC/view/TablaAmortizacionView.php
@@ -94,8 +94,6 @@
        
       <form action="<?php echo $helper->url("TablaAmortizacion","index"); ?>" method="post" enctype="multipart/form-data"  class="col-lg-12" style=" padding-bottom:300px;">
             <br>
-          
-           <?php if (!empty($resultRes)) {  foreach($resultRes as $resEdit) {?> 
    
 	         <div class="col-lg-12">
 	         <div class="panel panel-info">
@@ -107,14 +105,14 @@
   			 <div class="form-group" style="margin-top: 25px;">
 		    	<div class="col-xs-2 col-md-2" style="text-align: center;">
 			  	 <label for="ruc_clientes" class="control-label">Nro. Identificación:</label>
-			  	<input type="text"  name="ruc_clientes" id="ruc_clientes" value="<?php if ($sel_ruc_clientes!=""){echo $sel_ruc_clientes;} else {echo $resEdit->ruc_clientes;} ?>" class="form-control"/> 
+			  	<input type="text"  name="ruc_clientes" id="ruc_clientes" value="<?php if ($sel_ruc_clientes!="")  {echo $sel_ruc_clientes;} else { if (!empty($resultRes)) {  foreach($resultRes as $resEdit) {echo $resEdit->ruc_clientes;} }  }?>" class="form-control"/> 
 			   
             	</div>
             	</div>
 		   		<div class="form-group">
 		   		<div class="col-xs-4 col-md-4" style="text-align: center;">
 			  	<label for="razon_social_clientes" class="control-label">Razón Social:</label>
-			  	<input type="text"  name="razon_social_clientes" id="razon_social_clientes" value="<?php if ($sel_razon_social_clientes!=""){echo $sel_razon_social_clientes;} else {echo $resEdit->razon_social_clientes;} ?>" class="form-control"/> 
+			  	<input type="text"  name="razon_social_clientes" id="razon_social_clientes" value="<?php if ($sel_ruc_clientes!="")  {echo $sel_razon_social_clientes;} else { if (!empty($resultRes)) {  foreach($resultRes as $resEdit) {echo $resEdit->razon_social_clientes;} }  }?>" class="form-control"/> 
 			   	
             	</div>
               </div>
@@ -268,183 +266,7 @@
 			</div>
 	        
 	        
-	        
-	  
-          <?php } } else {?>
-           
-	         <div class="col-lg-12">
-	         <div class="panel panel-info">
-	         <div class="panel-heading">
-	         <h4><i class='glyphicon glyphicon-edit'></i> Tabla de Amortizacion</h4>
-	         </div>
-	         <div class="panel-body">
-  			 
-		     <div class="row">
-             <div class="form-group" style="margin-top: 25px;">
-		     <div class="col-xs-2 col-md-2" style="text-align: center;">
-			  	 <label for="ruc_clientes" class="control-label">Nro. Identificación:</label>
-			  	<input type="text"  name="ruc_clientes" id="ruc_clientes" value="<?php echo $sel_ruc_clientes ?>" class="form-control"/> 
-			   
-             </div>
-             </div>
-		     <div class="form-group">
-		     <div class="col-xs-4 col-md-4" style="text-align: center;">
-			  	<label for="razon_social_clientes" class="control-label">Razón Social:</label>
-			  	<input type="text"  name="razon_social_clientes" id="razon_social_clientes" value="<?php echo $sel_razon_social_clientes ?>" class="form-control"/> 
-			   	
-              </div>
-              </div>
-              
-		   
-		     <div class="col-xs-3 col-md-3">
-			    <input type="submit" id="buscar" name="buscar"  value="Buscar" class="btn btn-info " style="margin-top: 23px;"/> 	
-			     <button type="button" class="btn btn-warning glyphicon glyphicon-plus" data-toggle="modal" data-target="#myModal" style="margin-top: 20px;"></button>
-		
-		     </div>
-		     
-            
-		     </div>
-		     <br>
-		    <div class="row">
-		    <div class="col-xs-2 col-md-2">
-		    <div class="form-group">
-		    
-		     					  <label for="numero_credito_amortizacion_cabeza" class="control-label">Nro. Crédito:</label>
-                                  <input type="text" class="form-control" id="numero_credito_amortizacion_cabeza" name="numero_credito_amortizacion_cabeza" value="<?php echo $sel_numero_credito_amortizacion_cabeza	; ?>"  placeholder="#">
-                                  <span class="help-block"></span>
-		    </div>
-		    </div>
-		    <div class="col-xs-2 col-md-2">
-		    <div class="form-group">
-		    
-		     					  <label for="numero_pagare_amortizacion_cabeza" class="control-label">Nro. Pagare:</label>
-                                  <input type="text" class="form-control" id="numero_pagare_amortizacion_cabeza" name="numero_pagare_amortizacion_cabeza" value="<?php echo $sel_numero_pagare_amortizacion_cabeza; ?>"  placeholder="#">
-                                  <span class="help-block"></span>
-		    </div>
-		    </div>
-			  <div class="col-xs-2 col-md-2">
-		    <div class="form-group">
-					            <label for="id_tipo_creditos" class="control-label">Tipo Crédito:</label>
-					           <select name="id_tipo_creditos" id="id_tipo_creditos"  class="form-control" >
-                                  <option value="" selected="selected">--Seleccione--</option>
-									<?php foreach($resultCre as $res) {?>
-										<option value="<?php echo $res->id_tipo_creditos; ?>" <?php if($sel_id_tipo_creditos==$res->id_tipo_creditos){echo "selected";}?>   ><?php echo $res->nombre_tipo_creditos; ?> </option>
-							        <?php } ?>
-								   </select> 
-                                  <span class="help-block"></span>	
-		     </div>
-		     </div>
-		    <div class="col-xs-2 col-md-2">
-		    <div class="form-group">
-		    
-		     					  <label for="capital_prestado_amortizacion_cabeza" class="control-label">Cap. Prestado:</label>
-                                  <input type="text" class="form-control" id="capital_prestado_amortizacion_cabeza" name="capital_prestado_amortizacion_cabeza" value="<?php echo $sel_capital_prestado_amortizacion_cabeza;?>"  placeholder="$">
-                                  <span class="help-block"></span>
-		    </div>
-		    </div>
-			<div class="col-xs-1 col-md-1">
-		    <div class="form-group">
-		    
-		     					  <label for="tasa_interes_amortizacion_cabeza" class="control-label">Tasa:</label>
-                                  <input type="text" class="form-control" id="tasa_interes_amortizacion_cabeza" name="tasa_interes_amortizacion_cabeza" value="<?php echo $sel_tasa_interes_amortizacion_cabeza;?>"  placeholder="%">
-                                  <span class="help-block"></span>
-		    </div>
-		    </div>
-		    <div class="col-xs-1 col-md-1">
-		    <div class="form-group">
-		    
-		     					  <label for="plazo_meses_amortizacion_cabeza" class="control-label">Plazo:</label>
-                                  <input type="text" class="form-control" id="plazo_meses_amortizacion_cabeza" name="plazo_meses_amortizacion_cabeza" value="<?php echo $sel_plazo_meses_amortizacion_cabeza;?>"  placeholder="#">
-                                  <span class="help-block"></span>
-		    </div>
-		    </div>
-		    <div class="col-xs-2 col-md-2">
-		    <div class="form-group">
-		    
-		     					  <label for="fecha_amortizacion_cabeza" class="control-label">Fecha: </label>
-                                  <input type="date" class="form-control" id="fecha_amortizacion_cabeza" name="fecha_amortizacion_cabeza" value="<?php echo $sel_fecha_amortizacion_cabeza; ?>" >
-                                  <span class="help-block"></span>
-		    </div>
-		    </div>
-			</div>
-			
-			
-			
-			
-			</div>
-			
-            <div class="row">
-			<div class="col-xs-12 col-md-12 col-lg-12" style="text-align: center;" > 
-            <div class="form-group">
-            					
-            					   <input type="submit" id="Generar" name="Generar"  value="Generar" class="btn btn-success " />
-            </div>
-            </div>
-            </div>
-            </div>
-	        </div>
-	        
-	        
-	        
-	         <div class="col-lg-12">
-	         <div class="panel panel-info">
-	         <div class="panel-body">
-	        <div class="row">
-		 	<div class="col-xs-2 col-md-2">
-		    <div class="form-group">
-		    
-		     					  <label for="interes_normal_mensual_amortizacion_cabeza" class="control-label">Int. Mensual:</label>
-                                  <input type="text" class="form-control" id="interes_normal_mensual_amortizacion_cabeza" name="interes_normal_mensual_amortizacion_cabeza" value="<?php echo $sel_interes_normal_mensual_amortizacion_cabeza; ?>"  placeholder="0.00" readonly>
-                                  <span class="help-block"></span>
-		    </div>
-		    </div>
-			<div class="col-xs-2 col-md-2">
-		    <div class="form-group">
-		    
-		     					  <label for="plazo_dias_amortizacion_cabeza" class="control-label">Plazo Dias:</label>
-                                  <input type="text" class="form-control" id="plazo_dias_amortizacion_cabeza" name="plazo_dias_amortizacion_cabeza" value="<?php echo $sel_plazo_dias_amortizacion_cabeza; ?>"  placeholder="#" readonly>
-                                  <span class="help-block"></span>
-		    </div>
-		    </div>
-			<div class="col-xs-2 col-md-2">
-		    <div class="form-group">
-		    
-		     					  <label for="cantidad_cuotas_amortizacion_cabeza" class="control-label">Can. Cuotas:</label>
-                                  <input type="text" class="form-control" id="cantidad_cuotas_amortizacion_cabeza" name="cantidad_cuotas_amortizacion_cabeza" value="<?php echo $sel_cantidad_cuotas_amortizacion_cabeza; ?>"  placeholder="#" readonly>
-                                  <span class="help-block"></span>
-		    </div>
-		    </div>
-		    <div class="col-xs-2 col-md-2">
-		    <div class="form-group">
-		    
-		     					  <label for="interes_mora_mensual_amortizacion_cabeza" class="control-label">Mora:</label>
-                                  <input type="text" class="form-control" id="interes_mora_mensual_amortizacion_cabeza" name="interes_mora_mensual_amortizacion_cabeza" value="<?php echo $sel_interes_mora_mensual_amortizacion_cabeza; ?>"  placeholder="0.00" readonly>
-                                  <span class="help-block"></span>
-		    </div>
-		    </div>
-			<div class="col-xs-2 col-md-2">
-		    <div class="form-group">
-		    
-		     					  <label for="interes_normal_mensual_amortizacion_cabeza" class="control-label">Mora. Mensual</label>
-                                  <input type="text" class="form-control" id="interes_normal_mensual_amortizacion_cabeza" name="interes_normal_mensual_amortizacion_cabeza" value="<?php echo $sel_interes_normal_mensual_amortizacion_cabeza; ?>"  placeholder="0.00" readonly>
-                                  <span class="help-block"></span>
-		    </div>
-		    </div>
-		     <div class="col-xs-2 col-md-2">
-		    <div class="form-group">
-		    
-		     					  <label for="cantidad_cuotas_amortizacion_cabeza" class="control-label">Valor Cuota:</label>
-                                  <input type="text" class="form-control" id="cantidad_cuotas_amortizacion_cabeza" name="cantidad_cuotas_amortizacion_cabeza" value="<?php echo $sel_cantidad_cuotas_amortizacion_cabeza; ?>"  placeholder="$" readonly>
-                                  <span class="help-block"></span>
-		    </div>
-		    </div>
-			</div>
-	         </div>
-		    </div>
-			</div>
-	     
-         <?php } ?>
-         <?php if(!empty($resultDatos)){?>
+	         <?php if(!empty($resultDatos)){?>
         
         <div class="col-lg-12 col-xs-6">
 		
